144d2578852145f2d5cda422c193b3b60e43e2ad
moyanzhang <786425238@qq.com>
2015-08-10 14:16:14 +0800

    add feature

--- app/src/main/java/com/example/ksdhc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ksdhc/myapplication/MainActivity.java
@@ -2,16 +2,55 @@ package com.example.ksdhc.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+    private ViewPager mViewPager;
+    private int []mImagIds = new int[]{R.drawable.guide_image1,
+            R.drawable.guide_image2,R.drawable.guide_image3};
+    private List<ImageView> mImages = new ArrayList<ImageView>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        mViewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ImageView mImageView = new ImageView(MainActivity.this);
+                mImageView.setImageResource(mImagIds[position]);
+                mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                container.addView(mImageView);
+                mImages.add(mImageView);
+                return mImageView;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(mImages.get(position));
+            }
+
+            @Override
+            public int getCount() {
+                return mImagIds.length;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+        });
     }
 
     @Override
@@ -20,22 +59,6 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
 
-
-
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
